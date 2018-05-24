937c70a2ff9aa94777c649b9457a9cdaaff5ab13
Ivan Napolskykh <38167469+vainia@users.noreply.github.com>
2018-05-24 22:10:24 +0200

    Diagram Ulama Folder Add

---
+++ C#(Extended)(2018)/DiagramUlama/DiagramUlama/DiagramUlama.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace DiagramUlama
+{
+    partial class DiagramUlama
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.getNum = new System.Windows.Forms.TextBox();
+            this.errMsg = new System.Windows.Forms.Label();
+            this.diagramUlamaBox = new System.Windows.Forms.PictureBox();
+            this.imgHandler = new System.Windows.Forms.Panel();
+            this.scrollBar = new System.Windows.Forms.HScrollBar();
+            ((System.ComponentModel.ISupportInitialize)(this.diagramUlamaBox)).BeginInit();
+            this.imgHandler.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // getNum
+            // 
+            this.getNum.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.getNum.Location = new System.Drawing.Point(643, 438);
+            this.getNum.Name = "getNum";
+            this.getNum.Size = new System.Drawing.Size(91, 20);
+            this.getNum.TabIndex = 2;
+            this.getNum.Text = "36";
+            this.getNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.getNum.TextChanged += new System.EventHandler(this.getNum_TextChanged);
+            // 
+            // errMsg
+            // 
+            this.errMsg.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.errMsg.AutoSize = true;
+            this.errMsg.ForeColor = System.Drawing.Color.Red;
+            this.errMsg.Location = new System.Drawing.Point(640, 422);
+            this.errMsg.Name = "errMsg";
+            this.errMsg.Size = new System.Drawing.Size(94, 13);
+            this.errMsg.TabIndex = 4;
+            this.errMsg.Text = "Type valid number";
+            this.errMsg.Visible = false;
+            // 
+            // diagramUlamaBox
+            // 
+            this.diagramUlamaBox.Location = new System.Drawing.Point(0, 0);
+            this.diagramUlamaBox.Name = "diagramUlamaBox";
+            this.diagramUlamaBox.Size = new System.Drawing.Size(724, 407);
+            this.diagramUlamaBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.diagramUlamaBox.TabIndex = 5;
+            this.diagramUlamaBox.TabStop = false;
+            // 
+            // imgHandler
+            // 
+            this.imgHandler.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.imgHandler.AutoScroll = true;
+            this.imgHandler.Controls.Add(this.diagramUlamaBox);
+            this.imgHandler.Location = new System.Drawing.Point(10, 12);
+            this.imgHandler.Name = "imgHandler";
+            this.imgHandler.Size = new System.Drawing.Size(724, 407);
+            this.imgHandler.TabIndex = 6;
+            // 
+            // scrollBar
+            // 
+            this.scrollBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scrollBar.Location = new System.Drawing.Point(9, 438);
+            this.scrollBar.Maximum = 10000;
+            this.scrollBar.Name = "scrollBar";
+            this.scrollBar.Size = new System.Drawing.Size(631, 17);
+            this.scrollBar.TabIndex = 7;
+            this.scrollBar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.scrollBar_Scroll);
+            // 
+            // DiagramUlama
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 470);
+            this.Controls.Add(this.scrollBar);
+            this.Controls.Add(this.imgHandler);
+            this.Controls.Add(this.errMsg);
+            this.Controls.Add(this.getNum);
+            this.Name = "DiagramUlama";
+            this.Text = "Diagram Ulama";
+            ((System.ComponentModel.ISupportInitialize)(this.diagramUlamaBox)).EndInit();
+            this.imgHandler.ResumeLayout(false);
+            this.imgHandler.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox getNum;
+        private System.Windows.Forms.Label errMsg;
+        private System.Windows.Forms.PictureBox diagramUlamaBox;
+        private System.Windows.Forms.Panel imgHandler;
+        private System.Windows.Forms.HScrollBar scrollBar;
+    }
+}
+
---
+++ C#(Extended)(2018)/DiagramUlama/DiagramUlama/DiagramUlama.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiagramUlama
+{
+    public partial class DiagramUlama : Form
+    {
+        public DiagramUlama()
+        {
+            InitializeComponent();
+        }
+
+        private Boolean[] makePrimeTable()
+        {
+            int num, arrSize;
+            if (Int32.TryParse(getNum.Text, out num) && num != 0)
+            {
+                errMsg.Visible = false;
+
+                arrSize = (int)Math.Pow(Math.Ceiling(Math.Sqrt((double)num)), 2) + 1;
+                Boolean[] primeArr = new Boolean[arrSize];
+                primeArr[0] = true;
+                primeArr[1] = true;
+
+                for (int i = 2; i < arrSize; ++i) if (!primeArr[i]) for(int j = i+i; j < arrSize; j += i) primeArr[j] = true;
+                return primeArr;
+            }
+            else errMsg.Visible = true;
+            return null;
+        }
+
+        private float midOf(float big, float small)
+        {
+            return (float)(0.5 * (big - small));
+        }
+
+        private Bitmap draw(Boolean[] primeArr)
+        {
+            Graphics g = Graphics.FromImage(new Bitmap(1,1));
+            Font fnt = new Font("Arial", 10);
+            SizeF sz = new SizeF(0, 0), tmp;
+
+            for (int num = (int)Math.Pow(10, (double)(primeArr.Count().ToString().Length - 1)) * primeArr.Count() > 9 ? Int32.Parse(primeArr.Count().ToString()[0] + "") : 1; num < primeArr.Count(); ++num) if ((tmp = g.MeasureString(num + "", fnt)).Width > sz.Width) sz = tmp;
+
+            double sqr = Math.Sqrt(2);
+            int w = (int)Math.Ceiling(sqr * (sz.Width + 4));
+            int h = (int)Math.Ceiling(sqr * (sz.Height + 4));
+            int marg = 6;
+            int arrSizeWH = (int)Math.Sqrt((double)(primeArr.Count())-1);
+            Bitmap bmp = new Bitmap(arrSizeWH * (w + marg) + marg, arrSizeWH * (h + marg) + marg);
+            g = Graphics.FromImage(bmp);
+            //g.Clear(Color.FromArgb(255, 240, 240));
+            Brush back = new SolidBrush(Color.Yellow);
+            Brush backNotPrim = new SolidBrush(Color.Gray);
+            Brush fore = new SolidBrush(Color.Red);
+            Pen border = new Pen(Color.Blue, 2);
+            Pen line = new Pen(Color.Green, 3);
+            float x, y;
+            int pos = arrSizeWH - 1, dir = 1;
+            int[] tmpGrid = new int[(int)Math.Pow(arrSizeWH, 2)];
+
+            for (int i = primeArr.Count()-1; i > 0; i--)
+            {
+                x = pos / arrSizeWH * (marg + w) + marg;
+                y = pos % arrSizeWH * (marg + h) + marg;
+                if(primeArr[i]) g.FillEllipse(backNotPrim, x, y, w, h);
+                else g.FillEllipse(back, x, y, w, h);
+                g.DrawEllipse(border, x, y, w, h);
+                g.DrawString(i + "", fnt, fore, midOf(w, sz.Width) + x, midOf(h, sz.Height) + y);
+        
+                //g.DrawLine(line, X, Y, X + w * px, Y + h * py);
+
+                tmpGrid[pos] = i;
+                if (pos == 0 || pos == arrSizeWH - 1 || pos == arrSizeWH * (arrSizeWH - 1) || pos == Math.Pow(arrSizeWH, 2) - 1 || tmpGrid[pos + dir] != 0) dir = dir == -1 ? -arrSizeWH : dir == -arrSizeWH ? 1 : dir == 1 ? arrSizeWH : -1;
+                pos += dir;
+            }
+            return bmp;
+        }
+
+        private void getNum_TextChanged(object sender, EventArgs e)
+        {
+            if (makePrimeTable() != null) diagramUlamaBox.Image = draw(makePrimeTable());
+        }
+
+        private void scrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            if (makePrimeTable() != null) diagramUlamaBox.Image = draw(makePrimeTable());
+            getNum.Text = scrollBar.Value + "";
+        }
+    }
+}
